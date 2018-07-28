f9e4aebcb0727d8c42285d900a6dfb38ef19d104
dydeepak97 <dydeepak97@example.com>
2018-07-29 02:30:58 +0530

    Youtube API Test

--- js/app/harmony.js
+++ js/app/harmony.js
@@ -1,9 +1,27 @@
 (function(){
 
-'use strict'
+'use strict';
 
 console.log("Harmony Module Created");
 
-angular.module('harmony', ['ui.router' ]);
+angular.module('harmony', ['ui.router'])
+.controller('youtubeTestController', youtubeTestcontroller);
+
+youtubeTestcontroller.$inject = ['$scope','videoSearchService'];
+
+function youtubeTestcontroller($scope,videoSearchService){
+  console.log("controller Lonch");
+  var list = this;
+
+  list.items = videoSearchService.addItem();
+
+  console.log("list is here");
+  console.log(list.items);
+
+
+}
+
+
+
 
 })();
